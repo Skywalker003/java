6abf9ccdd93b4e32952adc506b418335c4d43531
Skywalker003 <srivikasramadas@gmail.com>
2024-12-01 15:24:33 +0530

    added

---
+++ java Learning/for_each.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class for_each {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int index = -1;
+        boolean found = false;
+
+        System.out.println("Enter the key value");
+        int key = sc.nextInt();
+
+        for (int num : arr){
+
+            if (num==key){
+                found = true ;
+                break;
+            }
+        }
+        if (found){
+            System.out.println("key is found");
+        }else {
+            System.out.println("key is not found");
+        }
+    }
+}
