b07132baa0836cbe562f21dc694f4e0089bc183d
Skywalker003 <srivikasramadas@gmail.com>
2025-05-18 17:59:22 +0530

    added

--- java Learning/Fibonacci.java
+++ java Learning/Fibonacci.java
@@ -23,6 +23,7 @@ public class Fibonacci {
 
  */
 
+/*
 import java.util.Scanner;
 
 public class Fibonacci {
@@ -45,6 +46,31 @@ public class Fibonacci {
             cur=next;
             System.out.print(","+next);
             get_fibonacci(num-1,prev,cur,next);
+        }
+    }
+}
+*/
+
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+        get_fibonacci(n);
+
+    }
+    static void get_fibonacci(int n){
+        int f[] = new int[n+1];
+        f[0]=0;
+        f[1]=1;
+        System.out.print(f[0]+","+f[1]);
+
+        for (int i =2;i<n;i++){
+            f[i]=f[i-1] + f[i-2];
+            System.out.print(","+ f[i]);
         }
     }
 }
