7d4ee7d0f009fde9a4b9b70ddf47405ad08f1a07
Skywalker003 <srivikasramadas@gmail.com>
2025-12-02 21:44:36 +0530

    added

--- java Learning/egcode.java
+++
@@ -1,6 +0,0 @@
-public class egcode {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
-
---
+++ java dsa/patterns/Square.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Square {
+    public static void main(String[] args){
+        for (int i = 0 ; i<4 ; i++){
+            for (int j = 0 ; j<4 ; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
