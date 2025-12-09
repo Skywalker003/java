090a695cf2f472d91b49899eac161a053b57b6fb
Skywalker003 <srivikasramadas@gmail.com>
2025-12-09 12:27:36 +0530

    added

--- java_dsa/patterns/p_20.java
+++ java_dsa/patterns/p_20.java
@@ -1,11 +1,12 @@
 public class p_20 {
     public static void main(String[] args) {
         int n =5;
+        //int space =n*2-2
 
         for(int i=1 ;i<=n*2-1 ;i++){
 
-            int space = i<n ? n*2-i*2 : i%n*2 ;
-            int star = i<n ? i : n-(i%n);
+            int space = i<n ? n*2-i*2 : i%n*2 ; // space -=2 : space+=2;
+            int star = i<n ? i : n-(i%n);//2*n-i
             for(int j=0 ;j<star ;j++){
                 System.out.print("*");
             }
