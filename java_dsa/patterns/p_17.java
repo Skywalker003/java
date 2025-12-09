511311cbd5ac4fd3baf505bff7c6e68722e0598b
Skywalker003 <srivikasramadas@gmail.com>
2025-12-09 10:05:34 +0530

    added

--- java_dsa/patterns/p_17.java
+++ java_dsa/patterns/p_17.java
@@ -6,14 +6,15 @@ public class p_17 {
             for(int j=0;j<n-i-1;j++){
                 System.out.print(" ");
             }
-            /*
+            
             for(char k='A';k<'A'+i;k++){
                 System.out.print(k);
             }
-            for(char k=(char)('A'+i-2);k>='A';k--){
+            for(char k=(char)('A'+ i);k>='A';k--){
                 System.out.print(k);
             }
-            */
+            
+           /*
            char ch = 'A';
            int bp = ((i*2)+1)/2;
             for(int k=1;k<=i*2+1;k++){
@@ -24,6 +25,7 @@ public class p_17 {
                     ch--;
                 }
             }
+            */
             System.out.println();
         }
     }
